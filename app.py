c460f69c9c44833fff7613a7977f5b0d83bcfe92
Syed Shahid Nazeer <nazeershahid256@gmail.com>
2026-01-12 21:23:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,6 @@ def main() -> None:
         {get_main_dashboard_html()}
         
         <!-- Projects View -->
-        <!-- Projects View -->
         {get_projects_view_html()}
 
         <!-- Education View -->
